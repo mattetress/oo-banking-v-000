0cb224ca3921252ef0a008a8a1ce251740ce6603
Matt Etress <mattetress@me.com>
2018-10-11 02:05:22 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,9 +18,13 @@ class Transfer
 
   def execute_transaction
     if @sender_beginning_balance == @sender.balance
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
+      if @sender.valid?
+        @sender.balance -= @amount
+        @receiver.balance += @amount
+        @status = "complete"
+      else
+        "Transaction rejected. Please check your account balance."
+      end
     end
   end
 
