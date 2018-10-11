baaf5b5ed2d4a655afea29a94bfe06834534bb3d
Matt Etress <mattetress@me.com>
2018-10-11 02:18:09 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,8 +2,6 @@ class Transfer
   attr_reader :sender, :receiver, :amount, :sender_beginning_balance
   attr_accessor :status
 
-
-
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
@@ -29,11 +27,10 @@ class Transfer
   def execute_transaction
     if @status == "pending" && funds_available?
       send
-      else
-        @status = "rejected"
-        "Transaction rejected. Please check your account balance."
-      end
-    
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
   end
 
   def reverse
